ce533c61b3bb3d101d7f8616e43a6558f1c0a4c5
DESKTOP-2U1QHMF\lomo1 <lomo1298@gmail.com>
2016-12-11 01:23:04 +0100

    gass conc measurement working

---
+++ arduino/gyro_acc/gyro_acc.ino
@@ -0,0 +1,15 @@
+
+
+int intPin = 8;
+int anScl = 5;
+int anSda = 4;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
